f052b8a614110128339fe1af4ae681197605abbd
Xav W <xavierwemmert@live.fr>
2022-11-17 22:47:12 +0100

    Drapaux

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="./logo192.png" alt="logo drapeaux" />
+      <h3>World's Flags</h3>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil officiis
+        debitis asperiores exercitationem magnam nesciunt, fugit quam aut saepe
+        praesentium! Atque accusantium, ipsa nesciunt quis quasi, libero nihil
+        nobis asperiores, eligendi aliquam architecto? A ad, nostrum laboriosam
+        accusantium magnam numquam nulla. Aliquid ut obcaecati cumque similique
+        voluptate error amet ipsam libero! Velit earum minus culpa quia
+        voluptatum exercitationem non vel, in magnam vitae sint a obcaecati
+        suscipit, sed deleniti eos, adipisci fuga! Corrupti enim repudiandae
+        dolor pariatur, ipsam sit maiores esse consequuntur et quod neque labore
+        laborum veniam ut tempore sapiente molestias dolores, suscipit cum
+        officia recusandae ad, minima mollitia!
+      </p>
+    </div>
+  );
+};
+
+export default About;
