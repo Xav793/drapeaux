70c1e47692f419c5a1218522e7e203b8e02ce7a1
Xavier Wemmert <94449267+Xav793@users.noreply.github.com>
2022-11-18 15:15:37 +0100

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,17 +9,11 @@ const About = () => {
       <Navigation />
       <h1>A propos</h1>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil officiis
-        debitis asperiores exercitationem magnam nesciunt, fugit quam aut saepe
-        praesentium! Atque accusantium, ipsa nesciunt quis quasi, libero nihil
-        nobis asperiores, eligendi aliquam architecto? A ad, nostrum laboriosam
-        accusantium magnam numquam nulla. Aliquid ut obcaecati cumque similique
-        voluptate error amet ipsam libero! Velit earum minus culpa quia
-        voluptatum exercitationem non vel, in magnam vitae sint a obcaecati
-        suscipit, sed deleniti eos, adipisci fuga! Corrupti enim repudiandae
-        dolor pariatur, ipsam sit maiores esse consequuntur et quod neque labore
-        laborum veniam ut tempore sapiente molestias dolores, suscipit cum
-        officia recusandae ad, minima mollitia!
+        Projet perso pour mon apprentissage de React.
+    
+    Xavier Wemmert.
+    xavportfolio.epizy.com
+    xavierwemmert@live.fr
       </p>
     </div>
   );
